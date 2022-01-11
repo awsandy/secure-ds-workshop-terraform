2ec8c52b50c2a9489bdac8700a14aba55ae92a42
awsandy <awsandy@amazon.com>
2022-01-11 16:27:17 +0000

    get bucket for sc

---
+++ 04-service-catalog/get-bucket-name.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Exit if any of the intermediate steps fail
+#set -e
+terraform state show data.aws_region.current 2&> /dev/null
+if [[ $? -eq 0 ]];then
+    treg=$(terraform state show data.aws_region.current | grep name | cut -f2 -d '=' | tr -d ' |"')
+else
+    treg=$(grep tagregion vars.tf | cut -f2 -d '=' | cut -f2 -d '"')    
+fi
+
+#echo $treg $tprof
+#t2=`date +%s%N`
+#t1=`hostname | cut -f1 -d'.'`
+#BUCKET_NAME=`printf "tf-state-%s-%s" $t1 $t2 | awk '{print tolower($0)}'`
+
+BUCKET_NAME=$(aws s3 ls --region $treg | grep secure-data-science-cloudformation- | cut -f3 -d ' ')
+jq -n --arg bn "$BUCKET_NAME" '{"Name":$bn}'
