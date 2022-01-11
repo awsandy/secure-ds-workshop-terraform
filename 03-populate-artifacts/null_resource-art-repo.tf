73c838fd41e36d388df2e9c527d849ee18a9f183
awsandy <awsandy@amazon.com>
2022-01-11 16:07:40 +0000

    aritfact fix

--- 03-populate-artifacts/null_resource-art-repo.tf
+++ 03-populate-artifacts/null_resource-art-repo.tf
@@ -2,7 +2,7 @@ resource "null_resource" "art-repo" {
   triggers = {
     always_run = timestamp()
   }
-  #depends_on = [aws_eks_cluster.cluster]
+  
   provisioner "local-exec" {
     on_failure  = fail
     when        = create
@@ -24,18 +24,16 @@ resource "null_resource" "art-repo" {
         echo $prf 
         echo $rg
       
-        
 
-        aws --profile ${var.profile} --region ${data.aws_region.current.name} codeartifact associate-external-connection \
+        aws --region ${data.aws_region.current.name} codeartifact associate-external-connection \
         --domain ${var.art-domain} \
         --domain-owner ${data.aws_caller_identity.current.account_id} \
         --repository ${var.ds-public-upstream-repo} \
         --external-connection ${var.external-conn}
-
-        
+  
 
         # Login to CodeArtifact which will setup pip.conf in $HOME/.config/pip/pip.conf
-        aws --profile ${var.profile} --region ${data.aws_region.current.name} codeartifact login --tool pip \
+        aws  --region ${data.aws_region.current.name} codeartifact login --tool pip \
         --domain ${var.art-domain} \
         --domain-owner ${data.aws_caller_identity.current.account_id} \
         --repository ${var.ds-shared-repo}
@@ -50,14 +48,13 @@ resource "null_resource" "art-repo" {
         --only-binary=:all: \
         numpy==1.20.3 pandas==1.2.5 protobuf==3.17.3 pyarrow==4.0.1 scikit-learn==0.24.2 scipy==1.7.0 psycopg2-binary xgboost==1.4.2
 
-        
 
         # Disassociate external public PyPI connection to restrict package download from public PYPI repo.
         # AWS CodeArtifact service as of this writing by default will download packages from public PYPI repo
         # if an external connection exists.
         # The connection can be associated again to install new packages in the shared repo by using:
         # aws codeartifact associate-external-connection
-        aws --profile ${var.profile} --region ${data.aws_region.current.name} codeartifact disassociate-external-connection \
+        aws --region ${data.aws_region.current.name} codeartifact disassociate-external-connection \
         --domain ${var.art-domain} \
         --domain-owner ${data.aws_caller_identity.current.account_id} \
         --repository ${var.ds-public-upstream-repo} \
