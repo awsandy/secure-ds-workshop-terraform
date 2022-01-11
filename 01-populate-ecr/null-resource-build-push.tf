dca40977d82883f8d5bfd32d92a3584f1abddd92
awsandy <awsandy@amazon.com>
2022-01-11 14:50:21 +0000

    ecr pop

--- 01-populate-ecr/null-resource-build-push.tf
+++ 01-populate-ecr/null-resource-build-push.tf
@@ -2,7 +2,7 @@ resource "null_resource" "build-push" {
   triggers = {
     always_run = timestamp()
   }
-  depends_on = [data.aws_ecr_repository.repo]
+  depends_on = [aws_ecr_repository.ds-shared-container-images]
   provisioner "local-exec" {
     on_failure  = fail
     when        = create
@@ -15,12 +15,12 @@ resource "null_resource" "build-push" {
       DISPLAY_NAME="Custom TensorFlow v2.4.1 Image"
 
       aws --profile ${var.profile} --region ${data.aws_region.current.name} ecr get-login-password | docker login --username AWS \
-        --password-stdin ${data.aws_ecr_repository.repo.repository_url}
+        --password-stdin ${aws_ecr_repository.ds-shared-container-images.repository_url}
       # Build the docker image and push to Amazon ECR (modify image tags and name as required)
       echo "docker build"
-      docker build . -t ${var.image_name} -t ${data.aws_ecr_repository.repo.repository_url}:${var.image_name}
+      docker build . -t ${var.image_name} -t ${aws_ecr_repository.ds-shared-container-images.repository_url}:${var.image_name}
       echo "docker push"
-      docker push ${data.aws_ecr_repository.repo.repository_url}:${var.image_name}
+      docker push ${aws_ecr_repository.ds-shared-container-images.repository_url}:${var.image_name}
       echo "************************************************************************************"
 
      EOT
