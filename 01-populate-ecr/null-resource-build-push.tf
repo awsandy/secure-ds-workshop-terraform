4ba54157e8137ed0d41d32ca0b2f4cb34839546d
awsandy <awsandy@amazon.com>
2022-01-11 15:00:01 +0000

    rm profile form aws cli

--- 01-populate-ecr/null-resource-build-push.tf
+++ 01-populate-ecr/null-resource-build-push.tf
@@ -14,7 +14,7 @@ resource "null_resource" "build-push" {
       
       DISPLAY_NAME="Custom TensorFlow v2.4.1 Image"
 
-      aws --profile ${var.profile} --region ${data.aws_region.current.name} ecr get-login-password | docker login --username AWS \
+      aws --region ${data.aws_region.current.name} ecr get-login-password | docker login --username AWS \
         --password-stdin ${aws_ecr_repository.ds-shared-container-images.repository_url}
       # Build the docker image and push to Amazon ECR (modify image tags and name as required)
       echo "docker build"
