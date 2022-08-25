4f8607364aef8531684af48c34de5e0b408cc6c2
awsandy <awsandy@amazon.com>
2022-08-25 16:00:15 +0100

    up

--- .tools/install-tf.sh
+++ .tools/install-tf.sh
@@ -2,5 +2,6 @@ echo "Terraform"
 wget -q https://releases.hashicorp.com/terraform/1.2.8/terraform_1.2.8_linux_amd64.zip
 unzip -qq terraform_1.2.8_linux_amd64.zip
 sudo cp -f terraform /usr/local/bin/
-sudo mv -f terraform /usr/bin/
+sudo cp -f terraform /usr/bin/
 rm -f terraform_1.2.8_linux_amd64.zip
+rm -f terraform
