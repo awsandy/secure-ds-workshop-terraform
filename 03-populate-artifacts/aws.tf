758c8f852467b07302a0b6f52fabe5904b2dbcb8
Andy Thomas <awsandy@amazon.co.uk>
2023-10-04 13:17:16 +0100

    up

--- 03-populate-artifacts/aws.tf
+++ 03-populate-artifacts/aws.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.5.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.27.0"
+      version = "~> 5.17.0"
     }
   }
 }
