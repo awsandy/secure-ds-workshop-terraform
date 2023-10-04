c8ad46a568b6424b34f764cd98661a6bb6040781
Andy Thomas <awsandy@amazon.co.uk>
2023-10-04 22:21:31 +0100

    up

--- 00-cloudformation/copy-cf-tos3.sh
+++ 00-cloudformation/copy-cf-tos3.sh
@@ -7,7 +7,12 @@ if [[ -z "$reg" ]]; then
     reg=$(aws configure get region)
 fi
 buck=$(aws s3 ls | grep secure-data-science-cloudformation | grep $reg | cut -f3 -d ' ')
+## sed stuff
 if [[ ! -z $buck ]]; then
+    sed -i.orig -e "s/secure-data-science-cloudformation-24780-eu-west-2/$buck/g" ds_environment-tf.yaml
+    sed -i.orig -e "s/secure-data-science-cloudformation-24780-eu-west-2/$buck/g" c1edf71b7274c2a72ca89d61f3de202d-tf.template
+    sed -i.orig -e "s/secure-data-science-cloudformation-24780-eu-west-2N/$buck/g" ds_env_studio_user_profile_v1-tf.yaml
+
     aws ssm put-parameter --name ds-s3-cloudformation-bucket --value $buck --type String --overwrite
     aws s3 cp ds_environment-tf.yaml s3://$buck/quickstart/ds_environment-tf.yaml
     aws s3 cp c1edf71b7274c2a72ca89d61f3de202d-tf.template s3://$buck/quickstart/c1edf71b7274c2a72ca89d61f3de202d-tf.template
